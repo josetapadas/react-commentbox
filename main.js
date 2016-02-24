303482332aeecdc94fda3e8c472d4b4a8adf399e
José Alves <jose.tapadas@gmail.com>
2016-02-24 14:53:50 +0000

    removed unused code

--- main.js
+++ main.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox';
 
-var sample_data = [
-  {id: 1, author: "Sandi Metz", text: "This controller is smaller than 100 lines."},
-  {id: 2, author: "Luis Rocha", text: "This is taum baum."},
-  {id: 3, author: "Rui Serra", text: "Merda"}
-];
-
 ReactDOM.render(
-  <CommentBox data={sample_data}/>,
+  <CommentBox />,
   document.getElementById('content')
 );
