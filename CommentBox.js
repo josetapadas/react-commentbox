1752f8b1dbd7451d81d09e52d38837a7a5ecd5f5
José Alves <jose.tapadas@gmail.com>
2016-02-24 13:38:40 +0000

    simple messaging system integration with firebase

--- CommentBox.js
+++ CommentBox.js
@@ -1,14 +1,48 @@
 import React from 'react';
 
 class CommentBox extends React.Component {
+  // getInitialState() executes exactly once during the lifecycle of the component and sets up the initial state of the component.
+  // The API is mostly what you would expect, with the exception of getInitialState(), this should be handled using the constructor
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+    this.dataItems = []
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+  }
+
+  componentWillMount() {
+    this.firebaseRef = new Firebase("https://jtreactcomments.firebaseio.com/comments");
+
+    this.firebaseRef.on("child_added", function(dataSnapshot) {
+      this.dataItems.push(dataSnapshot.val());
+
+      this.setState({
+        data: this.dataItems
+      });
+    }.bind(this));
+  }
+
+  componentDidMount() {
+    this.setState({
+      data: []
+    });
+  }
+
   render() {
     return (
       <div className="commentBox">
         <h1>Comments:</h1>
-        <CommentList data={this.props.data}/>
-        <CommentForm />
+        <CommentList data={this.state.data}/>
+        <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
       </div>
-    )
+    );
+  }
+
+  // event handlers
+  handleCommentSubmit(comment) {
+
+    // set values on firebase
+    this.firebaseRef.push(comment);
   }
 }
 
@@ -31,12 +65,53 @@ class CommentList extends React.Component {
 }
 
 class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // set initial state
+    this.state = {
+      author: '',
+      text: ''
+    }
+
+    // bind event handlers
+    this.handleAuthorChange = this.handleAuthorChange.bind(this)
+    this.handleTextChange = this.handleTextChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
   render() {
     return (
-      <div className="commentForm">
-        [I am a comment form.]
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Username" value={this.state.author} onChange={this.handleAuthorChange} />
+        <input type="text" placeholder="Write yor comment." value={this.state.text} onChange={this.handleTextChange} />
+        <input type="submit" value="Comment!" />
+      </form>
     )
+  }
+
+  // event handlers
+  handleAuthorChange(e) {
+    this.setState({ author : e.target.value });
+  }
+
+  handleTextChange(e) {
+    this.setState({ text : e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+
+    if( !author || !text ) {
+      return;
+    }
+
+    // trigger custom event
+    this.props.onCommentSubmit({ author: author, text: text });
+    this.setState({ author: '', text: '' });
   }
 }
 
@@ -45,7 +120,7 @@ class Comment extends React.Component {
     return (
       <div className="Comment">
         <div className="commentAuthor">
-          <b>{this.props.author}:</b>
+          <b>{this.props.author}</b> said:
         </div>
         { this.props.children }
       </div>
--- main.js
+++ main.js
@@ -4,7 +4,8 @@ import CommentBox from './CommentBox';
 
 var sample_data = [
   {id: 1, author: "Sandi Metz", text: "This controller is smaller than 100 lines."},
-  {id: 2, author: "Luis Rocha", text: "This is taum baum."}
+  {id: 2, author: "Luis Rocha", text: "This is taum baum."},
+  {id: 3, author: "Rui Serra", text: "Merda"}
 ];
 
 ReactDOM.render(
